ecfbd77cc871a973edf71591178d354aa6678441
Gsoyeb <golamsoyeb@gmail.com>
2021-04-11 14:05:15 +0100

    Added both repository

---
+++ app/src/main/java/uk/ac/le/co2103/hw4/ProductRepository.java
@@ -0,0 +1,96 @@
+package uk.ac.le.co2103.hw4;
+
+import android.app.Application;
+import android.os.AsyncTask;
+
+import androidx.lifecycle.LiveData;
+
+import java.util.List;
+
+public class ProductRepository {
+    private ProductDao productDao;
+    private LiveData<List<Product>> allProducts;
+
+    public ProductRepository(Application application){
+        ShoppingDatabase db = ShoppingDatabase.getInstance(application);
+        productDao = db.productDao();
+        allProducts = productDao.getAllProducts();
+    }
+
+    public LiveData<List<Product>> getAllProducts() {
+        return allProducts;
+    }
+
+    public void insert(Product product){
+        new InsertProductAsyncTask(productDao).execute(product);
+    }
+
+    public void update(Product product){
+        new UpdateProductAsyncTask(productDao).execute(product);
+    }
+
+    public void delete(Product product){
+        new DeleteProductAsyncTask(productDao).execute(product);
+    }
+
+    public void deleteAllProducts(){
+        new DeleteAllProductsAsyncTask(productDao).execute();
+    }
+
+    //AsyncTask-->Background Thread
+    private static class InsertProductAsyncTask extends AsyncTask<Product, Void, Void>{
+        private ProductDao productDao;
+
+        public InsertProductAsyncTask(ProductDao productDao) {
+            this.productDao = productDao;
+        }
+
+        @Override
+        protected Void doInBackground(Product... products) {
+            this.productDao.insert(products[0]);;
+            return null;
+        }
+    }
+
+    private static class UpdateProductAsyncTask extends AsyncTask<Product, Void, Void>{
+        private ProductDao productDao;
+
+        public UpdateProductAsyncTask(ProductDao productDao) {
+            this.productDao = productDao;
+        }
+
+        @Override
+        protected Void doInBackground(Product... products) {
+            this.productDao.update(products[0]);;
+            return null;
+        }
+    }
+
+    private static class DeleteProductAsyncTask extends AsyncTask<Product, Void, Void>{
+        private ProductDao productDao;
+
+        public DeleteProductAsyncTask(ProductDao productDao) {
+            this.productDao = productDao;
+        }
+
+        @Override
+        protected Void doInBackground(Product... products) {
+            this.productDao.delete(products[0]);;
+            return null;
+        }
+    }
+
+    private static class DeleteAllProductsAsyncTask extends AsyncTask<Void, Void, Void>{
+        private ProductDao productDao;
+
+        public DeleteAllProductsAsyncTask(ProductDao productDao) {
+            this.productDao = productDao;
+        }
+
+        @Override
+        protected Void doInBackground(Void... voids) {
+            this.productDao.deleteAllProducts();;
+            return null;
+        }
+    }
+}
---
+++ app/src/main/java/uk/ac/le/co2103/hw4/ShoppingListRepository.java
@@ -0,0 +1,99 @@
+package uk.ac.le.co2103.hw4;
+
+import android.app.Application;
+import android.os.AsyncTask;
+
+import androidx.lifecycle.LiveData;
+
+import java.util.List;
+
+public class ShoppingListRepository {
+    private ShoppingListDao shoppingListDao;
+    private LiveData<List<ShoppingList>> allShoppingLists;
+
+    public ShoppingListRepository(Application application){
+        ShoppingDatabase db = ShoppingDatabase.getInstance(application);
+        shoppingListDao = db.shoppingListDao();
+        allShoppingLists = shoppingListDao.getAllShoppingLists();
+    }
+
+    public LiveData<List<ShoppingList>> getAllShoppingLists() {
+        return allShoppingLists;
+    }
+
+
+    public void insert(ShoppingList shoppingList){
+        new InsertShoppingListAsyncTask(shoppingListDao).execute(shoppingList);
+    }
+
+    public void update(ShoppingList shoppingList){
+        new UpdateShoppingListAsyncTask(shoppingListDao).execute(shoppingList);
+    }
+
+    public void delete(ShoppingList shoppingList){
+        new DeleteShoppingListAsyncTask(shoppingListDao).execute(shoppingList);
+    }
+
+    public void deleteAllShoppingLists(){
+        new DeleteAllShoppingListsAsyncTask(shoppingListDao).execute();
+    }
+
+
+    //AsyncTask-->Background Thread
+    private static class InsertShoppingListAsyncTask extends AsyncTask<ShoppingList, Void, Void>{
+        private ShoppingListDao shoppingListDao;
+
+        private InsertShoppingListAsyncTask(ShoppingListDao shoppingListDao) {
+            this.shoppingListDao = shoppingListDao;
+        }
+
+        @Override
+        protected Void doInBackground(ShoppingList... shoppingLists) {
+            this.shoppingListDao.insert(shoppingLists[0]);
+            return null;
+        }
+    }
+
+    private static class UpdateShoppingListAsyncTask extends AsyncTask<ShoppingList, Void, Void>{
+        private ShoppingListDao shoppingListDao;
+
+        private UpdateShoppingListAsyncTask(ShoppingListDao shoppingListDao) {
+            this.shoppingListDao = shoppingListDao;
+        }
+
+        @Override
+        protected Void doInBackground(ShoppingList... shoppingLists) {
+            this.shoppingListDao.update(shoppingLists[0]);
+            return null;
+        }
+    }
+
+    private static class DeleteShoppingListAsyncTask extends AsyncTask<ShoppingList, Void, Void>{
+        private ShoppingListDao shoppingListDao;
+
+        private DeleteShoppingListAsyncTask(ShoppingListDao shoppingListDao) {
+            this.shoppingListDao = shoppingListDao;
+        }
+
+        @Override
+        protected Void doInBackground(ShoppingList... shoppingLists) {
+            this.shoppingListDao.delete(shoppingLists[0]);
+            return null;
+        }
+    }
+
+    private static class DeleteAllShoppingListsAsyncTask extends AsyncTask<Void, Void, Void>{
+        private ShoppingListDao shoppingListDao;
+
+        private DeleteAllShoppingListsAsyncTask(ShoppingListDao shoppingListDao) {
+            this.shoppingListDao = shoppingListDao;
+        }
+
+        @Override
+        protected Void doInBackground(Void... voids) {
+            this.shoppingListDao.deleteAllShoppingLists();
+            return null;
+        }
+    }
+
+}
