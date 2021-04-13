b2fd9396d7b155add6a29f6499832c87241b1d7b
Gsoyeb <golamsoyeb@gmail.com>
2021-04-13 16:24:38 +0100

    Added a toast and ID passed is right

--- app/src/main/java/uk/ac/le/co2103/hw4/ShoppingListActivity.java
+++ app/src/main/java/uk/ac/le/co2103/hw4/ShoppingListActivity.java
@@ -8,7 +8,9 @@ import androidx.recyclerview.widget.RecyclerView;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Toast;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import uk.ac.le.co2103.hw4.Adapter.ProductAdapter;
@@ -19,12 +21,20 @@ public class ShoppingListActivity extends AppCompatActivity {
     public static final String EXTRA_ID = "uk.edu.le.co2103.hw4.ID";
 
     private ProductViewModel productViewModel;
+    private int sizeProducts;
+    private int ID;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_shopping_list);
 
+        Intent intent = getIntent();
+        ID = intent.getIntExtra(EXTRA_ID,-1);
+        if (ID == -1){
+            Toast.makeText(ShoppingListActivity.this, "ID issue",Toast.LENGTH_SHORT).show();
+        }
+        Toast.makeText(ShoppingListActivity.this, "Id = "+String.valueOf(ID),Toast.LENGTH_SHORT).show();
 
         RecyclerView recyclerView = findViewById(R.id.recycler_view_product);
         recyclerView.setLayoutManager(new LinearLayoutManager(ShoppingListActivity.this));
@@ -32,6 +42,7 @@ public class ShoppingListActivity extends AppCompatActivity {
 
         final ProductAdapter adapter = new ProductAdapter();
         recyclerView.setAdapter(adapter);
+        sizeProducts = adapter.getItemCount();
 
         productViewModel = new ViewModelProvider(this).get(ProductViewModel.class);
         productViewModel.getAllProducts().observe(this, new Observer<List<Product>>() {
@@ -41,13 +52,12 @@ public class ShoppingListActivity extends AppCompatActivity {
             }
         });
 
-        setProducts();
 
     }
 
-    public void setProducts(){
-        Intent intent = getIntent();
-        int id = intent.getIntExtra(EXTRA_ID,-1);
-//        for (Product product : productViewModel.getAllProducts()){} //problem. Need to do it from the DAO
-    }
+//    public void setProducts(){
+//        Intent intent = getIntent();
+//        int id = intent.getIntExtra(EXTRA_ID,-1);
+////        for (Product product : productViewModel.getAllProducts()){} //problem. Need to do it from the DAO
+//    }
 }
