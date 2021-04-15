f342922fda73412e2e6b2465cfbd8741def865e2
Gsoyeb <golamsoyeb@gmail.com>
2021-04-15 09:02:45 +0100

    Created add interface

---
+++ app/src/main/java/uk/ac/le/co2103/hw4/CreateListActivity.java
@@ -0,0 +1,14 @@
+package uk.ac.le.co2103.hw4;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class CreateListActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_create_list);
+    }
+}
